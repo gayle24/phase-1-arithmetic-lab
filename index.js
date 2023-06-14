fa9ded6141ec03fd90f56d8f76759c5e84c88159
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-06-14 22:22:26 +0300

    arithnetic lab completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31, num2 = 2 ;
+let multiply = num1 * num2
+console.log (multiply)
+
+let random = Math.random();
+console.log(random);
+
+let num3 = 16, num4 = 6
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(4, 8, 12, 16, 20);
+console.log(max)
